ff896766f4f09ff588a05290ab295c7cc1a6e750
Danendra Hardjo Judanto <code.hardnead@gmail.com>
2025-11-28 21:24:47 +0700

    Add HelloWorld.java to Java Dasar Folder

---
+++ Java Dasar/HelloWorld.java
@@ -0,0 +1,10 @@
+package Java_Dasar;
+public class HelloWorld {
+    public static void main(String[] args){
+        // Code Program
+        System.out.println("\nHello Institut Teknologi Bandung");
+        System.out.println("Hallo nama aku Danendra Hardjo Judanto");
+        System.out.println("Halo aku sedang berkuliah di Jawa Barat\n");
+        System.out.println("Aku adalah lulusan dari Institut Teknologi Bandung dan Sekolah Tinggi Ilmu Statistika\n");
+    }
+}
